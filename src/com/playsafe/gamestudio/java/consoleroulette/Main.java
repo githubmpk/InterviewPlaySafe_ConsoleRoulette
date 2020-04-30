4129bad2a9b8ac3df243db257199ab4124242625
Kirthan <kirthan.puttegowda@pathcare.org>
2020-04-30 12:13:10 +0200

    input file modified

--- src/com/playsafe/gamestudio/java/consoleroulette/Main.java
+++ src/com/playsafe/gamestudio/java/consoleroulette/Main.java
@@ -3,6 +3,7 @@ package com.playsafe.gamestudio.java.consoleroulette;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        java.util.Timer timer = new java.util.Timer();
+        timer.schedule(new com.playsafe.gamestudio.java.consoleroulette.Timer(), 0, 30000);
     }
 }
